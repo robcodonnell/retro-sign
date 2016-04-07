39c1a03d6427dea4d062d5a286c09c297610073b
robcodonnell <robodonnell@Robs-MacBook-Pro.local>
2016-04-07 15:29:22 -0400

    most of it

---
+++ js/main.js
@@ -0,0 +1,9 @@
+var $lines = $('.lines');
+var $engage = $('.engage');
+var $btn = $('.btn');
+
+$btn.on('click', function (e) {
+  $('.engage').toggleClass('engaged');
+  $btn.toggleClass('button');
+  $lines.toggleClass('is-clicked');
+});
